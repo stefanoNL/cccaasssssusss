942feee815c9f794761e8d63b0f7f721e7f53228
stefanoNL <31925547+stefanoNL@users.noreply.github.com>
2017-11-10 11:48:37 +0100

    programma casus1

---
+++ Srs systeem/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Srs_systeem
+{
+    [Activity(Label = "Srs_systeem", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
